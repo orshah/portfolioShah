3d37efabbd06fa68183813686eac0fc33ace74c5
orshah <orshpov@gmail.com>
2023-06-29 00:53:43 -0500

    upd

--- src/sections/_e-learning/course/details/ElearningCourseDetailsHeroTwo.js
+++ src/sections/_e-learning/course/details/ElearningCourseDetailsHeroTwo.js
@@ -66,6 +66,16 @@ export default function ElearningCourseDetailsHero({ course }) {
 
   const isMdUp = useResponsive('up', 'md');
 
+  // const [openVideo, setOpenVideo] = useState(false);
+
+  // const handleOpenVideo = () => {
+  //   setOpenVideo(true);
+  // };
+
+  // const handleCloseVideo = () => {
+  //   setOpenVideo(false);
+  // };
+
   return (
     <>
       <Box
@@ -88,17 +98,23 @@ export default function ElearningCourseDetailsHero({ course }) {
           />
 
           <Grid container spacing={{ xs: 5, md: 10 }} direction="row-reverse">
-            <iframe
+            {/* <iframe
               title="something"
               width="400"
               height="450"
               src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FTL4ZW14DDKjvK8ozSb7A4s%2FDEALERSHIP%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D5-20557%26viewport%3D543%252C361%252C0.33%26scaling%3Dscale-down%26starting-point-node-id%3D5%253A20557%26mode%3Ddesign"
               allowfullscreen
-            />
+            /> */}
 
             <Grid xs={12} md={7}>
               <Stack spacing={3}>
                 <Stack spacing={2} alignItems="flex-start">
+                  {bestSeller && (
+                    <Label color="warning" variant="filled" sx={{ textTransform: 'uppercase' }}>
+                      Best Seller
+                    </Label>
+                  )}
+
                   <Typography variant="overline" sx={{ color: 'secondary.main' }}>
                     {category}
                   </Typography>
